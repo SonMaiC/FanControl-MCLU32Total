ad3af6c41a109fef0844fee5249e3554f3418f1e
Mai Sơn <mcshp95@gmail.com>
2024-05-29 23:31:09 +0700

    Add project files.

---
+++ FFU/Library/MCLU32.cs
@@ -0,0 +1,379 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FFU
+{
+    public class MCLU32
+    {
+        #region Constructor
+        public MCLU32(string portName)
+        {
+            sp.PortName = portName;
+            sp.BaudRate = 9600;
+            sp.Parity = Parity.None;
+            sp.StopBits = StopBits.One;
+            sp.DataBits = 8;
+            sp.Encoding = Encoding.GetEncoding(28591);
+            sp.ReadTimeout = 1000;
+        }
+        #endregion
+
+        #region Fields
+        private Mutex comLock = new Mutex();
+        private SerialPort sp = new SerialPort();
+        public bool IsConnected = false;
+
+        /// <summary>
+        /// Lower speed limit value 0~1000rpm
+        /// </summary>
+        private int dataLSV = 0x00;
+        public int DataLSV
+        {
+            get { return dataLSV; }
+            set
+            {
+                if (value < dataHSV) dataLSV = dataHSV;
+                if (value < 0) dataLSV = 0;
+                dataLSV = value / 10;
+            }
+        }
+        /// <summary>
+        /// Upper speed limit value 0~1000rpm
+        /// </summary>
+        private int dataHSV = 0x64;
+        public int DataHSV
+        {
+            get { return dataHSV; }
+            set
+            {
+                if (value < dataLSV) dataHSV = dataLSV;
+                if (value > 1000) dataHSV = 1000 / 10;
+                dataHSV = value / 10;
+            }
+        }
+        #endregion
+
+        private enum Mode1
+        {
+            GroupRead = 0x8E,
+            GroupWrite = 0x8D,
+            BlockRead = 0x8A,
+            BlockWrite = 0x89,
+            Reset = 0x99
+        }
+        private enum Mode2
+        {
+            GroupRead = 0x83,
+            GroupWrite = 0x9F,
+            BlockRead = 0x9F,
+            BlockWrite = 0xBF,
+            Reset = 0xBF
+        }
+
+        public void Connect()
+        {
+            if (sp.IsOpen) return;
+
+            try
+            {
+                sp.Open();
+                if (sp.IsOpen) { IsConnected = true; }
+            }
+            catch (Exception)
+            {
+                IsConnected = false;
+            }
+        }
+        public void Disconnect()
+        {
+            if (!sp.IsOpen) return;
+            sp.Close();
+            IsConnected = false;
+        }
+        public bool GroupWrite(int nSpeed)
+        {
+            return GroupWrite(0, nSpeed);
+        }
+        public bool GroupWrite(int nMCULID, int nSpeed)
+        {
+            if (!sp.IsOpen) return false;
+            if (nMCULID < 0 || nMCULID >= 32) return false;
+
+            int mculID = nMCULID;
+            byte[] packet = new byte[10];
+            int dataSV = (nSpeed / 10);
+            byte checksum = 0;
+
+            packet[0] = 0x02;
+            packet[1] = (byte)Mode1.GroupWrite;
+            packet[2] = (byte)Mode2.GroupWrite;
+            packet[3] = (byte)(0x81 + mculID);
+            packet[4] = 0x9F;
+            packet[5] = (byte)dataSV;
+            packet[6] = (byte)dataLSV;
+            packet[7] = (byte)dataHSV;
+            packet[9] = 0x03;
+            for (int i = 1; i < 8; i++)
+            {
+                checksum += packet[i];
+            }
+            packet[8] = checksum;
+
+            comLock.WaitOne();
+            try
+            {
+                sp.Write(packet, 0, packet.Length);
+                byte[] resByte = new byte[256];
+                int length = sp.Read(resByte, 0, resByte.Length);
+                if (length <= 0) return false;
+                if (resByte[1] == (byte)Mode1.GroupWrite &&
+                    resByte[2] == (byte)Mode2.GroupWrite &&
+                    resByte[3] == (byte)(0x81 + mculID) &&
+                    resByte[5] == 0xB9)
+                    return true;
+                else
+                    return false;
+            }
+            catch { return false; }
+            finally
+            {
+                comLock.ReleaseMutex();
+            }
+        }
+        public bool BlockWrite(int nLCUID, int nSpeed)
+        {
+            return BlockWrite(0, nLCUID, nSpeed);
+        }
+        public bool BlockWrite(int nMCULID, int nLCUID, int nSpeed)
+        {
+            if (!sp.IsOpen) return false;
+            if (nMCULID < 0 || nMCULID >= 32 || nLCUID < 0 || nLCUID > 32) return false;
+
+            int mculID = nMCULID;
+            int lcuID = nLCUID;
+            byte[] packet = new byte[12];
+            int dataSV = (nSpeed / 10);
+            byte checksum = 0;
+
+            packet[0] = 0x02;
+            packet[1] = (byte)Mode1.BlockWrite;
+            packet[2] = (byte)Mode2.BlockWrite;
+            packet[3] = (byte)(0x81 + mculID);
+            packet[4] = 0x9F;
+            packet[5] = (byte)(0x81 + lcuID);
+            packet[6] = (byte)(0x81 + lcuID);
+            packet[7] = (byte)dataSV;
+            packet[8] = (byte)dataLSV;
+            packet[9] = (byte)dataHSV;
+            packet[11] = 0x03;
+            for (int i = 1; i < 10; i++)
+            {
+                checksum += packet[i];
+            }
+            packet[10] = checksum;
+
+            comLock.WaitOne();
+            try
+            {
+                sp.Write(packet, 0, packet.Length);
+                byte[] resByte = new byte[256];
+                int length = sp.Read(resByte, 0, resByte.Length);
+                if (length <= 0) return false;
+                if (resByte[1] == (byte)Mode1.GroupWrite &&
+                    resByte[2] == (byte)Mode2.GroupWrite &&
+                    resByte[3] == (byte)(0x81 + mculID) &&
+                    resByte[5] == 0xB9)
+                    return true;
+                else
+                    return false;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            finally
+            {
+                comLock.ReleaseMutex();
+            }
+        }
+        public bool GroupRead(ref int[] nSpeed, ref int[] nStatus)
+        {
+            return GroupRead(0, ref nSpeed, ref nStatus);
+        }
+        public bool GroupRead(int nMCULID, ref int[] nSpeed, ref int[] nStatus)
+        {
+            if (!sp.IsOpen) return false;
+            if (nMCULID < 0 || nMCULID >= 32) return false;
+
+            int[] retSpeed = new int[32];
+            int[] retStatus = new int[32];
+            int mculID = nMCULID;
+            byte[] packet = new byte[7];
+            byte checksum = 0;
+
+            packet[0] = 0x02;
+            packet[1] = (byte)Mode1.GroupRead;
+            packet[2] = (byte)Mode2.GroupRead;
+            packet[3] = (byte)(0x081 + mculID);
+            packet[4] = 0x9F;
+            packet[6] = 0x03;
+            for (int i = 1; i < 5; i++)
+            {
+                checksum += packet[i];
+            }
+            packet[5] = checksum;
+
+            comLock.WaitOne();
+            try
+            {
+                sp.Write(packet, 0, packet.Length);
+                byte[] resByte = new byte[256];
+                int length = sp.Read(resByte, 0, resByte.Length);
+                if (length <= 0) return false;
+                if (resByte[1] == (byte)Mode1.GroupWrite &&
+                    resByte[2] == (byte)Mode2.GroupWrite &&
+                    resByte[3] == (byte)(0x81 + mculID))
+                {
+                    for (int j = 0; j < 32; j++)
+                    {
+                        retSpeed[j] = resByte[6 + j];
+                        retStatus[j] = resByte[7 + j];
+                    }
+                    return true;
+                }
+                else
+                {
+                    Array.Fill(retSpeed, 0);
+                    Array.Fill(retStatus, 0);
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                Array.Fill(retSpeed, 0);
+                Array.Fill(retStatus, 0);
+                return false;
+            }
+            finally
+            {
+                nSpeed = retSpeed;
+                nStatus = retStatus;
+                comLock.ReleaseMutex();
+            }
+        }
+        public bool BlockRead(int nLCUID, ref int nPV, ref int nSV, ref int nST)
+        {
+            return BlockRead(0, nLCUID, ref nPV, ref nSV, ref nST);
+        }
+        public bool BlockRead(int nMCULID, int nLCUID, ref int nPV, ref int nSV, ref int nST)
+        {
+            if (!sp.IsOpen) return false;
+            if (nMCULID < 0 || nMCULID >= 32 || nLCUID < 0 || nLCUID > 32) return false;
+
+            int pv = 0, sv = 0, st = 0;
+            int mculID = nMCULID;
+            int lcuID = nLCUID;
+            byte[] packet = new byte[9];
+            byte checksum = 0;
+
+            packet[0] = 0x02;
+            packet[1] = (byte)Mode1.BlockRead;
+            packet[2] = (byte)Mode2.BlockRead;
+            packet[3] = (byte)(0x81 + mculID);
+            packet[4] = 0x9F;
+            packet[5] = (byte)(0x81 + lcuID);
+            packet[6] = (byte)(0x81 + lcuID);
+            packet[8] = 0x03;
+            for (int i = 1; i < 7; i++)
+            {
+                checksum += packet[i];
+            }
+            packet[7] = checksum;
+
+            comLock.WaitOne();
+            try
+            {
+                sp.Write(packet, 0, packet.Length);
+                byte[] resByte = new byte[256];
+                int length = sp.Read(resByte, 0, resByte.Length);
+                if (length <= 0) return false;
+                if (resByte[1] == (byte)Mode1.BlockRead &&
+                    resByte[2] == (byte)Mode2.BlockRead &&
+                    resByte[3] == (byte)(0x81 + mculID) &&
+                    resByte[5] == (byte)(0x81 + lcuID))
+                {
+                    pv = resByte[6];
+                    st = resByte[7];
+                    sv = resByte[8];
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                nPV = pv;
+                nSV = sv;
+                nST = st;
+                comLock.ReleaseMutex();
+            }
+        }
+        public bool Reset(int nLCUID)
+        {
+            return Reset(0, nLCUID);
+        }
+        public bool Reset(int nMCULID, int nLCUID)
+        {
+            if (!sp.IsOpen) return false;
+            if (nMCULID < 0 || nMCULID >= 32 || nLCUID < 0 || nLCUID > 32) return false;
+
+            int mculID = nMCULID;
+            int lcuID = nLCUID;
+            byte[] packet = new byte[10];
+            byte checksum = 0;
+
+            packet[0] = 0x02;
+            packet[1] = (byte)Mode1.Reset;
+            packet[2] = (byte)Mode2.Reset;
+            packet[3] = (byte)(0x81 + mculID);
+            packet[4] = 0x9F;
+            packet[5] = (byte)(0x81 + lcuID);
+            packet[6] = (byte)(0x81 + lcuID);
+            packet[7] = 0xF5;
+            packet[9] = 0x03;
+            for (int i = 1; i < 8; i++)
+            {
+                checksum += packet[i];
+            }
+            packet[8] = checksum;
+
+            comLock.WaitOne();
+            try
+            {
+                sp.Write(packet, 0, packet.Length);
+                byte[] resByte = new byte[256];
+                int length = sp.Read(resByte, 0, resByte.Length);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            finally
+            {
+                comLock.ReleaseMutex();
+            }
+        }
+    }
+}
---
+++ FFU/Library/Melsec.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Animation;
+using System.Threading;
+
+namespace FFU
+{
+    public class Melsec
+    {
+        public enum DeviceType : byte
+        {
+            M = 0x90,
+            L = 0x92,
+            D = 0xA8,
+            ZR = 0xB0,
+        }
+
+        #region Fields
+        private TcpClient tcpClient;
+        private NetworkStream ns;
+        private int port;
+        private string ip;
+
+        private bool IsPLCConnected = false;
+        private Mutex plcLock = new Mutex();
+
+        #endregion
+
+        #region Methods
+        private void PLCOpen()
+        {
+            if (IsPLCConnected) return;
+            try
+            {
+                tcpClient = new TcpClient();
+                tcpClient.Connect(IPAddress.Parse(ip), port);
+                if (tcpClient != null && tcpClient.Connected)
+                {
+                    ns = tcpClient.GetStream();
+                    IsPLCConnected = true;
+                }
+                //log.WriteLog("PLC Connection Opened!", LOG.LogType.PLC);
+            }
+            catch (Exception ex)
+            {
+                IsPLCConnected = false;
+                //log.WriteLog("PLC Open Fail:" + ex.Message.ToString(), LOG.LogType.PLC);
+            }
+        }
+
+        private void PLCClose()
+        {
+            if (IsPLCConnected) return;
+            if (ns != null)
+            {
+                ns.Dispose();
+                ns = null;
+            }
+            if (tcpClient != null)
+            {
+                tcpClient.Close();
+            }
+
+            IsPLCConnected = false;
+            //log.WriteLog("PLC Connection Closed!", LOG.LogTyPe.PLC);
+        }
+
+        private void PLCReadDeviceBlock(int nStartAddr, int nLength, ref int[] refData)
+        {
+            plcLock.WaitOne();
+            int[] returnValue = new int[nLength];
+          
+          byte[] sendByte = new byte[21 + nLength * 2];
+            byte[] MCLength = BitConverter.GetBytes(nLength * 2 + 12);
+            byte[] DataLength = BitConverter.GetBytes(nLength);
+            byte[] Byteadd = BitConverter.GetBytes(nStartAddr);
+
+            sendByte[0] = 0x50; //SubHeader
+            sendByte[1] = 0x00; //SubHeader
+            sendByte[2] = 0x00; //Access Route-StationNo
+            sendByte[3] = 0xFF; //AccessRoute-Network No - 00: Network| FF: PC No
+            sendByte[4] = 0xFF; //Access Route-StationNo
+            sendByte[5] = 0x03; //Access Route-StationNo
+            sendByte[6] = 0x00;
+            sendByte[7] = 0x0C;
+            sendByte[8] = 0X00;
+            sendByte[9] = 0x10;
+            sendByte[10] = 0x00;
+            sendByte[11] = 0x01; //Command //read
+            sendByte[12] = 0x04; //Command //read
+            sendByte[13] = 0x00; //Subcommand
+            sendByte[14] = 0x00; //SubCommand
+            sendByte[15] = Byteadd[0];
+            sendByte[16] = Byteadd[1];
+            sendByte[17] = Byteadd[2];
+            sendByte[18] = 0xA8; // device D
+            sendByte[19] = DataLength[0];
+            sendByte[20] = DataLength[1];
+            try
+            {
+                ns.Write(sendByte, 0, 21);
+                ns.Flush();
+                ns.ReadTimeout = 1000;
+
+                byte[] readByte = new byte[256];
+                int length = ns.Read(readByte, 0, readByte.Length);
+                if (length >= 0)
+                {
+                    int index = 0;
+                    int num11 = ((readByte[8] << 8) + readByte[7]) - 2;
+                    int[] numArray = new int[num11 / 2];
+                    int num12 = 11;
+                    while (num12 < (11 + num11))
+                    {
+                        numArray[index] = readByte[num12 + 1] << 8;
+                        numArray[index] += (Int16)readByte[num12];
+                num12 += 2;
+                        index++;
+                    }
+                    returnValue = numArray;
+                }
+                else
+                {
+                    for (int i = 0; i < nLength; i++)
+                    {
+                        returnValue[i] = 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //log.WriteLog("Read Block Fail:" + ex.Message.ToString(), LOG.LogType.PLC);
+            }
+            finally
+            {
+                refData = returnValue;
+                plcLock.ReleaseMutex();
+            }
+        }
+
+        private void PLCWriteDevice(int nAddress, int nVal)
+        {
+            int[] val = new int[] { nVal };
+            PLCWriteDeviceBlock(nAddress, 1, val);
+        }
+
+        private void PLCWriteDeviceBlock(int nStartAddr, int nLength, int[] nVal)
+        {
+            if (!IsPLCConnected || ns == null) return;
+
+            plcLock.WaitOne();
+
+            byte[] sendByte = new byte[21 + nLength * 2];
+            byte[] mcLengthByte = BitConverter.GetBytes(12 + nLength * 2);
+            byte[] lengthByte = BitConverter.GetBytes(nLength);
+            byte[] addrByte = BitConverter.GetBytes(nStartAddr);
+
+            sendByte[0] = 80;
+            sendByte[1] = 0;
+            sendByte[2] = 0;
+            sendByte[3] = 0xFF;
+            sendByte[4] = 0xFF;
+            sendByte[5] = 3;
+            sendByte[6] = 0;
+            sendByte[7] = mcLengthByte[0];
+            sendByte[8] = mcLengthByte[1];
+            sendByte[9] = 10;
+            sendByte[10] = 0;
+            sendByte[11] = 1;
+            sendByte[12] = 20;
+            sendByte[13] = 0;
+            sendByte[14] = 0;
+            sendByte[15] = addrByte[0];
+            sendByte[16] = addrByte[1];
+            sendByte[17] = addrByte[2];
+            sendByte[18] = 0xA8;
+            sendByte[19] = lengthByte[0];
+            sendByte[20] = lengthByte[1];
+
+            for (int i = 0; i < nLength; i++)
+            {
+                byte[] data = BitConverter.GetBytes(nVal[i]);
+                sendByte[21 + i * 2] = data[0];
+                sendByte[21 + i * 2 + 1] = data[1];
+            }
+
+            try
+            {
+                ns.Write(sendByte, 0, 21 + nLength * 2);
+                ns.Flush();
+                byte[] readByte = new byte[256];
+                int length = ns.Read(readByte, 0, readByte.Length);
+            }
+            catch (Exception ex)
+            {
+                //log.WriteLog("Write Device Fail:" + ex.Message.ToString(), LOG.LogType.PLC);
+            }
+            finally
+            {
+                plcLock.ReleaseMutex();
+            }
+        }
+        #endregion
+    }
+}
